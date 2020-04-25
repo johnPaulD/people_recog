796367271d76c0f4ab0ee20811b55582339f2c50
johnPaulD <jpmendy@hotmail.co.uk>
2020-04-25 18:18:12 +0100

    Push test for video face detection

--- face_detection.py
+++ face_detection.py
@@ -1,43 +1,39 @@
 import sys
 import dlib
-from skimage import io
 import cv2 as cv
 
-# Take the image file name from the command line
-file_name = sys.argv[1]
-
-# Create a HOG face detector using the built-in dlib class
+## Libraries to download in requirements.txt
+'''
+dlib
+opencv-python
+'''
+## Prepare HOG face detector
 face_detector = dlib.get_frontal_face_detector()
 
-#win = dlib.image_window()
+## First get video streams up and running
+vid = cv.VideoCapture(0)
 
-# Load the image into an array
-image = io.imread(file_name)
+## get the frame and the faces from the frame
+while True:
+    ret, frame = vid.read()
+    faces = face_detector(frame, 1)
+    
+    ## 1 If there are faces show the boundary boxes over the face
+    if len(faces) > 0:
+        # Display total number of captured faces
+        total = "Total {}".format(len(faces))
+        font = cv.FONT_HERSHEY_PLAIN
+        color = (255,192,203)
+        frame = cv.putText(frame,total,(50,50),font,1,color,2)
+        for face in faces:
+            start = (face.left(),face.top())
+            end = (face.right(),face.bottom())
+            frame = cv.rectangle(frame, start,end,color,2)
+    #cv.windowResize
+    cv.imshow("Face Detection",frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
 
-# Run the HOG face detector on the image data.
-# The result will be the bounding boxes of the faces in our image.
-detected_faces = face_detector(image, 1)
-
-print("I found {} faces in the file {}".format(len(detected_faces), file_name))
-
-# Open a window on the desktop showing the image
-#win.set_image(image)
-
-
-# Loop through each face we found in the image
-for i, face_rect in enumerate(detected_faces):
-
-# Detected faces are returned as an object with the coordinates 
-# of the top, left, right and bottom edges
-print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
-img = cv.rectangle(cv.imread(file_name),(face_rect.left,face_rect.top),(face_rect.right,face_rect.bottom),(0,190,0),1)
-if img == None:
-cv.imshow("Faces", img)
-cv.imwrite('foo.jpg',img)
-cv.waitKey(0)
+#Gently exit
+vid.release()
 cv.destroyAllWindows()
-# Draw a box around each face we found
-#win.add_overlay(face_rect)
-	        
-# Wait until the user hits <enter> to close the window	        
-#dlib.hit_enter_to_continue()
